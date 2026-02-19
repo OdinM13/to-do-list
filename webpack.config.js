85c54cbbb20958bb8dbf953442ccc7f3b4160da5
OdinM13 <markodinius@gmail.com>
2026-02-19 22:36:35 +0100

    Build basic file and folder structure and set-up

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            favicon: './src/assets/favicon.png',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,   
+                use: ['style-loader', 'css-loader'], 
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    devServer: {
+        static: './dist',
+        watchFiles: ['./src/template.html'],
+    },
+};
